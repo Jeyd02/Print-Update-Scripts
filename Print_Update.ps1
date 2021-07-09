b86128a249cdb465fe2c5c97bf681a45d37c68fb
Jeyd02 <30659788+Jeyd02@users.noreply.github.com>
2021-07-09 18:00:50 -0400

    Update Print_Update.ps1

--- Print_Update.ps1
+++ Print_Update.ps1
@@ -35,7 +35,7 @@ if($os_ver -eq "10.0.14393"){
 if($os_ver -eq "10.0.17763"){
    # Executes on 1809 or Server 2019
    New-Item -Path C:\temp\patch\Default.txt -ItemType File -Force
-   Set-Service -Name "wuauserv" -StartupType Manua
+   Set-Service -Name "wuauserv" -StartupType Manual
    Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v NoWarningNoElevationOnInstall /t REG_DWORD /d 0 /f
    Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v NoWarningNoElevationOnUpdate /t REG_DWORD /d 0 /f
    Start-Process -FilePath "curl.exe" -ArgumentList '--insecure --ssl-no-revoke --url "http://download.windowsupdate.com/c/msdownload/update/software/secu/2021/07/windows10.0-kb5004947-x64_c00ea7cdbfc6c5c637873b3e5305e56fafc4c074.msu" -o "c:/temp/patch/kb5004947.msu"' -Wait
